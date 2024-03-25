7014db26e36f4ad4c4f5534fc6a575bbce9dc2e7
itztiva <98614842+itztiva@users.noreply.github.com>
2024-03-24 23:56:10 -0400

    more log

--- main/operations/contentpages.js
+++ main/operations/contentpages.js
@@ -1,12 +1,12 @@
 const Express = require('express');
 const express = Express();
+const log = require("../utils/base/log.js");
 const functions = require("../utils/functions/functions.js");
 const path = require("path");
 require('dotenv').config({ path: path.resolve(__dirname, '.', 'config', '.env')});
 
 express.get("/content/api/pages/fortnite-game", async (req,res) => {
     const c = functions.contentpages(req);
-
     res.json(c);
 });
 
--- main/utils/base/log.js
+++ main/utils/base/log.js
@@ -1,7 +1,7 @@
 class Logger {
 
     backend(...messages) {
-        console.log(`\x1b[37m[\x1b[34mBACKEND\x1b[0m\x1b[37m]`, ...messages);
+        console.log(`\x1b[37m[\x1b[35mBACKEND\x1b[0m\x1b[37m]`, ...messages);
     }
 
     debug(...messages) {
